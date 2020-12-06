04934e1d92e60a6060447f872bb403d1847efe7e
Isaac Sandoval <isaacgarvalena@gmail.com>
2020-12-06 16:57:33 -0600

    fix issue 3

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ require('./config/config');
 
 const express = require('express');
 const app = express();
+var nodemailer = require("nodemailer");
 
 const bodyParser = require('body-parser');
 
@@ -41,8 +42,10 @@ app.post("/send-email", async(req, res) => {
             res.status(500).send(error.message);
         }
 
-        return res.status(200).json(req.body);
+            res.status(200).json(req.body);
     });
+
+    return 'El corre ha sido enviado correctamente';
 })
 
 
