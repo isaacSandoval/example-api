5cafb59b2fc06f8d098d019d23ddbebb6b34f944
Isaac Sandoval <isaacgarvalena@gmail.com>
2020-12-06 17:48:42 -0600

    cors

--- server/server.js
+++ server/server.js
@@ -6,6 +6,14 @@ var nodemailer = require("nodemailer");
 
 const bodyParser = require('body-parser');
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
 
