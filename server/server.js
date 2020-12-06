5337d7ff7b71c820b1ede6a8c1405f135612d8de
Isaac Sandoval <isaacgarvalena@gmail.com>
2020-12-06 16:49:19 -0600

    changes

--- server/server.js
+++ server/server.js
@@ -11,42 +11,40 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
-
-app.get('/usuario', function(req, res) {
-    res.json('get Usuario LOCAL!!!');
+app.get('/', (req, resp) => {
+    res.send("API PARA ENVIAR CORREOS LANDING PAGE A UDOCS.TEAM@GMAIL.COM")
 });
 
-app.post('/usuario', function(req, res) {
+app.post("/send-email", async(req, res) => {
+    var name = req.body.name;
+    var email = req.body.email;
+    var message = req.body.message;
+    var phone = req.body.phone; 
 
-    let body = req.body;
-
-    if (body.nombre === undefined) {
-
-        res.status(400).json({
-            ok: false,
-            mensaje: 'El nombre es necesario'
-        });
-
-    } else {
-        res.json({
-            persona: body
-        });
-    }
-
-});
-
-app.put('/usuario/:id', function(req, res) {
-
-    let id = req.params.id;
-
-    res.json({
-        id
+    var transporter = nodemailer.createTransport({
+        service: "Gmail",
+        auth: {
+            user: "udocs.team@gmail.com",
+            pass: "udocs es el mejor"
+        }
     });
-});
 
-app.delete('/usuario', function(req, res) {
-    res.json('delete Usuario');
-});
+    var mailOptions = {
+        from: email,
+        to: "udocs.team@gmail.com",
+        subject: `Contacto U-DOCS - ${name}`,
+        text: `${message} \n \n ==============================   \n Nombre: ${name} \n Telefono: ${phone} \n Correo del cliente: ${email} \n ==============================  `
+    }
+ 
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            res.status(500).send(error.message);
+        }
+
+        return res.status(200).json(req.body);
+    });
+})
+
 
 app.listen(process.env.PORT, () => {
     console.log('Escuchando puerto: ', process.env.PORT);
